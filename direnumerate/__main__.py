16b336d992495b240debc7f60a259d15175f7e45
Juan Bindez <juanbindez780@gmail.com>
2023-09-27 11:35:00 -0300

    Update __main__.py

--- direnumerate/__main__.py
+++ direnumerate/__main__.py
@@ -21,7 +21,7 @@ class DirScan():
                     if response.status_code == 200:
                         print(Color.GREEN + f"target access [Found]: -> {Color.RESET + full_url}")
                     elif response.status_code == 204:
-                        print(Color.RED + f"target access [No Content]: -> {Color.RESET+ full_url}")
+                        print(Color.BLUE + f"target access [No Content]: -> {Color.RESET+ full_url}")
                     elif response.status_code == 400:
                         print(Color.YELLOW + f"target access [Bad Request]: -> {Color.RESET+ full_url}")
                     elif response.status_code == 401:
@@ -29,7 +29,7 @@ class DirScan():
                     elif response.status_code == 403:
                         print(Color.RED + f"target access [Forbidden]: -> {Color.RESET+ full_url}")
                     elif response.status_code == 404:
-                        print(Color.RED + f"target access [Not Found]: -> {Color.RESET+ full_url}")
+                        print(Color.YELLOW + f"target access [Not Found]: -> {Color.RESET+ full_url}")
                     elif response.status_code == 500:
                         print(Color.BLUE + f"target access [Internal Server Error]: -> {Color.RESET+ full_url}")
         except FileNotFoundError:
