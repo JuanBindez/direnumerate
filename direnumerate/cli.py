fbb1ae318f3bffcc19c74815e02173aebd12b2a3
JuanBindez <juanbindez780@gmail.com>
2023-09-27 20:03:47 -0300

    1.0-rc10

--- direnumerate/cli.py
+++ direnumerate/cli.py
@@ -5,31 +5,9 @@ from direnumerate.__main__ import PortScan
 from direnumerate.colors import Color
 from direnumerate.version import __version__
 
-def main():
+
+def dir_scan(args):
     try:
-        parser = argparse.ArgumentParser(description="Direnumerate - Directory Enumeration on Web Servers")
-        parser.add_argument("-u", "--url", required=True, help="Target URL (including scheme, e.g. http://www.example.com)")
-        parser.add_argument("-w", "--wordlist", required=True, help="wordlist file")
-        parser.add_argument("-V", "--version", required=True, help=__version__)
-        parser.add_argument("-p", "--portscan", required=True, help="port Scan")
-        parser.add_argument("-i", "--ip", required=True, help="ipaddress")
-        parser.add_argument("-s", "--startport", required=True, help="inicial port")
-        parser.add_argument("-e", "--endport", required=True, help="final port")
-
-
-        args = parser.parse_args()
-
-        if args.p:
-            ip = args.i
-            start_port = args.s
-            end_port = args.e
-
-            scan = PortScan(ip, start_port, end_port)
-            scan.open_ports()
-            pass
-        else:
-            pass
-
         url = args.url
         wordlist_file = args.wordlist
 
@@ -40,5 +18,43 @@ def main():
     except KeyboardInterrupt:
         print(Color.GREEN + "-------------- attempt interrupted by user ------------" + Color.RESET)
 
+
+def port_scan(args):
+    try:
+        host = args.target
+        start_port = args.start_port
+        end_port = args.end_port
+
+        scanner = PortScan(host, start_port, end_port)
+        scanner.scan_ports()
+    except KeyboardInterrupt:
+        print(Color.GREEN + "-------------- Port scan interrupted by user ------------" + Color.RESET)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Direnumerate - Directory Enumeration on Web Servers")
+    subparsers = parser.add_subparsers(title="subcommands")
+
+    dir_parser = subparsers.add_parser("dir-scan", help="Perform directory enumeration")
+    dir_parser.add_argument("-u", "--url", required=True, help="Target URL (including scheme, e.g. http://www.example.com)")
+    dir_parser.add_argument("-w", "--wordlist", required=True, help="wordlist file")
+    dir_parser.set_defaults(func=dir_scan)
+
+    port_parser = subparsers.add_parser("port-scan", help="Perform port scanning")
+    port_parser.add_argument("-t", "--target", required=True, help="Target host")
+    port_parser.add_argument("-s", "--start-port", required=True, type=int, help="Start port")
+    port_parser.add_argument("-e", "--end-port", required=True, type=int, help="End port")
+    port_parser.set_defaults(func=port_scan)
+
+    parser.add_argument("-V", "--version", required=True, help=__version__)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
 if __name__ == "__main__":
     main()
+
+
+
+
