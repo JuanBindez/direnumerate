9bf2600cb71f2e91ad7043da675a4f9df0fe84db
JuanBindez <juanbindez780@gmail.com>
2024-02-24 10:34:33 -0300

    update >> 2.3.0

--- direnumerate/__main__.py
+++ direnumerate/__main__.py
@@ -1,6 +1,6 @@
 # this is part of the Direnumerate project.
 #
-# Copyright ©  2023  Juan Bindez  <juanbindez780@gmail.com>
+# Copyright ©  2023 - 2024 Juan Bindez  <juanbindez780@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
