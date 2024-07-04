acab4fb205335f61ee5b731cda01579085aeb022
JuanBindez <juanbindez780@gmail.com>
2024-07-04 14:57:00 -0300

    direnumerate 4.1-rc3

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'direnumerate 4.1-rc2'
+git commit -m 'direnumerate 4.1-rc3'
 git push -u origin dev
-git tag v4.1-rc2
+git tag v4.1-rc3
 git push --tag
 make clean
 make upload
--- direnumerate/version.py
+++ direnumerate/version.py
@@ -1,5 +1,5 @@
 
-__version__ = "4.1-rc2"
+__version__ = "4.1-rc3"
 
 
 if __name__ == "__main__":
