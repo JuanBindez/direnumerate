1ec95e9f881ed2f8f7ceabc2be7acbf0044cc5a4
juan <juanbindez780@gmail.com>
2024-10-15 22:51:40 -0300

    Direnumerate 4.1.-rc4 update test

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+VERSION=4
+PATCHLEVEL=1
+SUBLEVEL=
+EXTRAVERSION="-rc4"
+
+BRANCH="dev"
+MESSAGE="update test"
+
+FULL_VERSION="$VERSION.$PATCHLEVEL.$SUBLEVEL$EXTRAVERSION"
+
 git add .
-git commit -m 'direnumerate 4.1-rc3'
-git push -u origin dev
-git tag v4.1-rc3
-git push --tag
+git commit -m "Direnumerate $FULL_VERSION $MESSAGE"
+git push -u origin $BRANCH
+git tag v$FULL_VERSION
+git push --tags
 make clean
 make upload
+
+echo "Build $FULL_VERSION completed successfully!"
